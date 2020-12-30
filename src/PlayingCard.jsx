1850a37a257d4b758524610e8dcee2d71e39c442
Leight Johnson <leightjohnson93@gmail.com>
2020-12-29 21:01:08 -0700

    no unused vars

--- src/PlayingCard.jsx
+++ src/PlayingCard.jsx
@@ -1,7 +1,4 @@
-// import { useEffect, useState } from 'react'
-import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
-import CardContent from '@material-ui/core/CardContent'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
